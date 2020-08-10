b96e565559213deb2eae24c6e39347dd60064960
limed <limed@mozilla.com>
2020-08-10 14:54:48 -0700

    Add helm provider

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -11,6 +11,17 @@ provider "kubernetes" {
   load_config_file       = false
 }
 
+provider "helm" {
+  version = "~> 1"
+
+  kubernetes {
+    host                   = data.aws_eks_cluster.itse-apps-prod-1.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.itse-apps-prod-1.certificate_authority.0.data)
+    token                  = data.aws_eks_cluster_auth.itse-apps-prod-1.token
+    load_config_file       = false
+  }
+}
+
 provider "random" {
   version = "~> 2"
 }
