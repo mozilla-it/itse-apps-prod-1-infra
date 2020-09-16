27cea6b446d3cbb66d46fdd70a26ead33f48d058
limed <limed@mozilla.com>
2020-09-16 12:43:55 -0700

    Add EIP for ingress-nginx

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -5,6 +5,15 @@ output "cluster_id" {
 output "cluster_oidc_issuer_url" {
   value = module.itse-apps-prod-1.cluster_oidc_issuer_url
 }
+
 output "cluster_name" {
   value = module.itse-apps-prod-1.cluster_id
+}
+
+output "refractr_eip_allocation_id" {
+  value = aws_eip.refractr_eip.*.id
+}
+
+output "refractr_ips" {
+  value = aws_eip.refractr_eip.*.public_ip
 }
