05017846ef84d0890907c0f335a6857fd5a37a47
Christina Harlow <cmharlow@gmail.com>
2021-06-15 17:49:44 -0400

    update refractr ingress-nginx helm to latest version

--- terraform/main.tf
+++ terraform/main.tf
@@ -47,6 +47,7 @@ module "itse-apps-prod-1" {
   external_secrets_settings   = local.external_secrets_settings
   node_groups                 = local.node_groups
   fluentd_papertrail_settings = local.fluentd_papertrail_settings
+  enable_logging              = true
   admin_users_arn             = ["arn:aws:iam::783633885093:role/maws-admin", "arn:aws:iam::517826968395:role/itsre-admin"]
 }
 
