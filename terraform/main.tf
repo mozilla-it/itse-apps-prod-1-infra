67bfd541240308fee6b0aeb57698fef8ff2d6965
Christina Harlow <cmharlow@gmail.com>
2021-06-19 19:44:10 -0400

    alphabetize within reason lists for legibility

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,27 +1,11 @@
 locals {
   cluster_features = {
-    "prometheus"         = true
+    "aws_calico"         = true
+    "external_secrets"   = true
+    "fluentd_papertrail" = true
     "flux"               = true
     "flux_helm_operator" = true
-    "external_secrets"   = true
-    "aws_calico"         = true
-    "fluentd_papertrail" = true
-  }
-
-  flux_settings = {
-    "git.url"    = "git@github.com:mozilla-it/itse-apps-prod-1-infra"
-    "git.branch" = "main"
-  }
-
-  node_groups = {
-    green_node_group = {
-      desired_capacity = 5,
-      min_capacity     = 5,
-      disk_size        = 120,
-      max_capacity     = 20,
-      instance_types   = ["m5.large"],
-      subnets          = data.terraform_remote_state.vpc.outputs.private_subnets
-    }
+    "prometheus"         = true
   }
 
   external_secrets_settings = {
@@ -32,6 +16,22 @@ locals {
     "externalSecrets.secretsKey" = "/prod/${module.itse-apps-prod-1.cluster_id}-papertrail"
   }
 
+  flux_settings = {
+    "git.url"    = "git@github.com:mozilla-it/itse-apps-prod-1-infra"
+    "git.branch" = "main"
+  }
+
+  node_groups = {
+    green_node_group = {
+      desired_capacity = 5,
+      disk_size        = 120,
+      instance_types   = ["m5.large"],
+      max_capacity     = 20,
+      min_capacity     = 5,
+      subnets          = data.terraform_remote_state.vpc.outputs.private_subnets
+    }
+  }
+
   subnet_az = [for s in data.aws_subnet.public : s.availability_zone]
   subnet_id = [for s in data.aws_subnet.public : s.id]
 }
@@ -39,16 +39,16 @@ locals {
 module "itse-apps-prod-1" {
   source                      = "github.com/mozilla-it/terraform-modules//aws/eks?ref=master"
   cluster_name                = "itse-apps-prod-1"
-  cluster_version             = "1.18"
-  vpc_id                      = data.terraform_remote_state.vpc.outputs.vpc_id
-  cluster_subnets             = data.terraform_remote_state.vpc.outputs.public_subnets
-  cluster_features            = local.cluster_features
-  flux_settings               = local.flux_settings
-  external_secrets_settings   = local.external_secrets_settings
-  node_groups                 = local.node_groups
-  fluentd_papertrail_settings = local.fluentd_papertrail_settings
-  enable_logging              = true
   admin_users_arn             = ["arn:aws:iam::783633885093:role/maws-admin", "arn:aws:iam::517826968395:role/itsre-admin"]
+  cluster_features            = local.cluster_features
+  cluster_subnets             = data.terraform_remote_state.vpc.outputs.public_subnets
+  cluster_version             = "1.18"
+  enable_logging              = true
+  external_secrets_settings   = local.external_secrets_settings
+  fluentd_papertrail_settings = local.fluentd_papertrail_settings
+  flux_settings               = local.flux_settings
+  node_groups                 = local.node_groups
+  vpc_id                      = data.terraform_remote_state.vpc.outputs.vpc_id
 }
 
 # Chicken and egg issue, this needs to exist first
