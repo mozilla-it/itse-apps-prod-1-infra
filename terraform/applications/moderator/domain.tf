6f3cb5a1c76b16ee9439e8dc2da24d5c45dc1b61
Christina Harlow <charlow@mozilla.com>
2021-07-27 11:16:28 -0400

    add moderator infrastructure, migrated from mozilla-it/moderator-infra

---
+++ terraform/applications/moderator/domain.tf
@@ -0,0 +1,11 @@
+# moderator.prod.mozit.cloud
+# -> lb, k8s via external-dns
+
+# moderator.mozilla.org
+# -> email / SES setup via terraform
+# -> lb, k8s via external-dns
+
+module "moderator_mozilla" {
+  source = "github.com/mozilla-it/terraform-modules//aws/dns/apex?ref=master"
+  domain = var.moderator_mozilla
+}
