b3cda6b6b202736fd5f470bc0ad33e93a4cfde21
Christina Harlow <charlow@mozilla.com>
2021-08-16 11:44:01 -0400

    discourse migrated & updated application tf baseline

---
+++ terraform/applications/discourse/variables.tf
@@ -0,0 +1,84 @@
+variable "cf_cache_compress" {
+  default = "true"
+  type    = string
+}
+
+variable "cf_price_class" {
+  default = "PriceClass_100"
+  type    = string
+}
+
+variable "cost_center" {
+  default = "1410"
+  type    = string
+}
+
+variable "discourse_cdn_zone" {
+  default = "discourse.mozilla.org"
+  type    = string
+}
+
+variable "discourse_mozilla" {
+  default = "discourse.mozilla.org"
+  type    = string
+}
+
+variable "environment" {
+  default = "prod"
+  type    = string
+}
+
+variable "project" {
+  default = "discourse"
+  type    = string
+}
+
+variable "project_desc" {
+  default = "discourse.mozilla.org"
+  type    = string
+}
+
+variable "project_email" {
+  default = "it-sre@mozilla.com"
+  type    = string
+}
+
+variable "psql_instance" {
+  default = "db.t2.micro"
+  type    = string
+}
+
+variable "psql_storage_allocated" {
+  default = 10
+  type    = number
+}
+
+variable "psql_storage_max" {
+  default = 100
+  type    = number
+}
+
+variable "psql_version" {
+  default = "10"
+  type    = string
+}
+
+variable "redis_instance" {
+  default = "cache.t2.small"
+  type    = string
+}
+
+variable "redis_num_nodes" {
+  default = 1
+  type    = number
+}
+
+variable "redis_version" {
+  default = "5.0.4"
+  type    = string
+}
+
+variable "region" {
+  default = "us-west-2"
+  type    = string
+}
