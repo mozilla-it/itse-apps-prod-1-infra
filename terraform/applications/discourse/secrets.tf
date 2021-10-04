a0633e3543dd65b7ed71cd6bf8fa4fddbe3dbeb5
Christina Harlow <charlow@mozilla.com>
2021-10-04 11:26:24 -0400

    make discourse logger config a templated secret

--- terraform/applications/discourse/secrets.tf
+++ terraform/applications/discourse/secrets.tf
@@ -4,3 +4,8 @@ resource "aws_secretsmanager_secret" "envvar" {
   name        = "/${var.environment}/discourse/envvar"
   description = "discourse ${var.environment} environment variables as json (see helm chart for expected json)"
 }
+
+resource "aws_secretsmanager_secret" "logger" {
+  name        = "/${var.environment}/discourse/logger"
+  description = "discourse ${var.environment} papertrail logger configuration as json (see helm chart for expected json)"
+}
